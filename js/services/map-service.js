84a4843c96177577f72927d2ccc06f7959dcdba9
eladgu <eladgube@gmail.com>
2020-04-19 18:09:06 +0300

    geo-api

--- js/services/map-service.js
+++ js/services/map-service.js
@@ -1,7 +1,43 @@
 
+const gAPI_KEY =AIzaSyBTPIWlmkZosY9bz2NO7Grw1V9SvtvDESc;
 
 
-
-
-// TODO: getFromGeo(API)
 // TODO: getFromGoogle(API)
+
+
+
+
+
+// getFromGeo(API)
+
+
+// API ADDRESS=> LAN ***get STRING***
+
+
+function getCord(address) {
+    $.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBTPIWlmkZosY9bz2NO7Grw1V9SvtvDESc`,
+     (res)=>{
+        console.log('Service Got:', res);
+    })
+}
+
+
+// API LAN=> ADDRESS
+
+
+function getAddress(lat,lng) {
+    $.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBTPIWlmkZosY9bz2NO7Grw1V9SvtvDESc`,
+     (res)=>{
+        console.log('Service Got:', res);
+    })
+}
+
+
+
+
+
+
+
+
+
+
